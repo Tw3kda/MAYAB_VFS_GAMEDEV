297b1afcc61d324029177022f9f15311d0979feb
Alumno1 <alumno1@anahuacmayab.edu.mx>
2026-07-08 17:56:13 -0600

    cositas

--- Assets/Scripts/Combat/HC_Bullet.cs
+++ Assets/Scripts/Combat/HC_Bullet.cs
@@ -1,4 +1,7 @@
+using System.Collections;
+using System.Drawing;
 using UnityEngine;
+using UnityEngine.ProBuilder;
 
 public class HC_Bullet : MonoBehaviour
 {
@@ -41,7 +44,8 @@ public class HC_Bullet : MonoBehaviour
         if (impactPrefab != null)
             this.impactEffectPrefab = impactPrefab;
 
-        rb.linearVelocity = transform.forward * speed;
+        //rb.linearVelocity = transform.forward * speed;
+        rb.linearVelocity = new Vector3(10f,10f,10f);
         timer = 0f;
     }
 
@@ -51,15 +55,17 @@ public class HC_Bullet : MonoBehaviour
         lifetime = defaultLifetime;
         knockbackForce = 0f;
         ownerPool = null;
-        rb.linearVelocity = transform.forward * defaultSpeed;
+        //rb.linearVelocity = transform.forward * defaultSpeed;
+        rb.linearVelocity = new Vector3(10f, 10f, 10f);
         timer = 0f;
     }
 
     void Update()
     {
+        Debug.Log(rb.linearVelocity.magnitude);
         timer += Time.deltaTime;
-        if (timer >= lifetime)
-            ReturnOrDestroy();
+        //if (timer >= lifetime)
+            //ReturnOrDestroy();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -72,26 +78,36 @@ public class HC_Bullet : MonoBehaviour
             Vector3 hitPoint = collision.contacts.Length > 0 ? collision.contacts[0].point : transform.position;
             Vector3 hitDir = transform.forward;
             target.TakeDamage(damage, hitPoint, hitDir);
-            pierceCount++;
-
-            if (knockbackForce > 0f)
-            {
-                Rigidbody targetRb = collision.gameObject.GetComponent<Rigidbody>();
-                if (targetRb != null)
-                    targetRb.AddForce(hitDir * knockbackForce, ForceMode.Impulse);
-            }
         }
 
         SpawnImpactEffect(collision);
-       if (pierceCount >= piercingTargets)
+    
+
+            //ReturnOrDestroy();
+     
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        
+        if (collider.tag == "Zombies")
         {
-            ReturnOrDestroy();
+            IDamageable target = collider.gameObject.GetComponent<IDamageable>();
+            target.TakeDamage(damage);
         }
-        else
-        {
-            return;
-        }
-            
+
+        //GameObject fx = Instantiate(impactEffectPrefab, gameObject.transform.position, Quaternion.identity);
+        //Destroy(fx, impactEffectDuration);
+
+        StartCoroutine(DestroyCoroutine());
+        //ReturnOrDestroy();
+
+    }
+
+    IEnumerator DestroyCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+        ReturnOrDestroy(); 
     }
 
     void SpawnImpactEffect(Collision collision)
@@ -105,7 +121,7 @@ public class HC_Bullet : MonoBehaviour
 
     void ReturnOrDestroy()
     {
-        rb.linearVelocity = Vector3.zero;
+        //rb.linearVelocity = Vector3.zero;
         if (ownerPool != null)
             ownerPool.Return(gameObject);
         else
